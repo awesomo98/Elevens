578c629255fcb6124485341e8c2f0c3adae67424
Bilal Reza <rezasb@s.dcsdk12.org>
2016-04-11 10:20:32 -0600

    Done with a1

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,16 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("king", "spades", 13);
+		Card card2 = new Card("jack", "diamonds", 11);
+		Card card3 = new Card("jack", "diamonds", 11);
+		
+		System.out.println(card1.pointValue());
+		System.out.println(card1.suit());
+		System.out.println(card1.rank());
+		System.out.println(card2.matches(card3));
+		System.out.println(card2.toString());
+		System.out.println(card3.matches(card1));
+		System.out.println(card3.rank());
 	}
 }
